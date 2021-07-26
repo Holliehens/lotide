83867c275ca1438540967f3e255a4e4bb880d99d
Hollie Hensman <hesnmanh@gmail.com>
2021-07-26 00:54:51 +0000

    Console.assert Passed/Failed

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log('✅ ✅ ✅ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🚫 🚫 🚫Assertion Failed: ' + actual +  ' !== ' + expected);
+  }
+
+
+
+};
+
+console.assert(assertEqual(' Lighthouse Labs', 'Bootcamp'));
+console.assert(assertEqual(1, 1));
