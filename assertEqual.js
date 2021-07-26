dc98d7fae6584da4c7d764781837b8bea21b480c
Hollie Hensman <hesnmanh@gmail.com>
2021-07-26 01:32:09 +0000

    Test. Changedto()

--- assertEqual.js
+++ assertEqual.js
@@ -10,5 +10,5 @@ const assertEqual = function(actual, expected) {
 
 };
 
-console.assert(assertEqual(' Lighthouse Labs', 'Bootcamp'));
+console.assert(assertEqual(" Lighthouse Labs", "Bootcamp"));
 console.assert(assertEqual(1, 1));
