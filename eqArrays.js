893908013456a2fccb600c55a92a53e932d19591
Hollie Hensman <hesnmanh@gmail.com>
2021-08-14 21:56:20 +0000

    Refactor module.exports, clean up code

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`🚫 🚫 🚫Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-
-
-};
-
-
-
-
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -27,9 +11,5 @@ const eqArrays = function(array1, array2) {
   return true;  
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1])); 
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+module.exports = eqArrays;
