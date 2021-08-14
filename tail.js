64b8eefe8b2286af41a959b8872e82700ace5ba3
Hollie Hensman <hesnmanh@gmail.com>
2021-08-14 21:38:45 +0000

    Refactor tail module.export, clean up of code

--- tail.js
+++ tail.js
@@ -1,14 +1,3 @@
-const assertEqual = function(param1, param2) {
-
-  if (param1 === param2) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${param1}  ===  ${param2}`);
-  } else {
-    console.log(`🚫 🚫 🚫Assertion Failed: ${param1} !== ${param2}`);
-  }
-
-
-
-};
 const tail = function(array) {
   let newArray = [];
   for (let i = 0; i < array.length; i++) {
@@ -21,9 +10,6 @@ const tail = function(array) {
   return newArray;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
 
+module.exports = tail;
 
