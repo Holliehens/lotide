0357f6b41d9e56df431d3bb1f386cdf18db2ac7d
Hollie Hensman <hesnmanh@gmail.com>
2021-08-14 22:51:17 +0000

    middle tests, require assertArraysEqual and eqArrays

---
+++ test/middleTest.js
@@ -0,0 +1,8 @@
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+
+middle([1, 2, 3, 4, 5, 6]);
+middle([1]); 
+middle([1, 2]);
+middle([1, 2, 3, 4, 5]);
