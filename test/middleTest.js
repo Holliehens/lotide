d27f7375e09048d05a51624574070c38bca04d68
Hollie Hensman <hesnmanh@gmail.com>
2021-08-15 18:08:28 +0000

    Refactor tests with Mocha & Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,17 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
 const eqArrays = require('../eqArrays');
+const oddArray = [33, 5, 8, 9, 63];
+const evenArray = [33, 5, 8, 9, 63, 89];
 
-middle([1, 2, 3, 4, 5, 6]);
-middle([1]); 
-middle([1, 2]);
-middle([1, 2, 3, 4, 5]);
+
+describe("#middle", () => {
+  it("returns [8] for [33, 5, 8, 9, 63]", () => {
+    assert.deepEqual(middle(oddArray), [8]);
+  });
+  it("returns [8, 9] for [33, 5, 8, 9, 63, 89]", () => {
+    assert.deepEqual(middle(evenArray), [8, 9]);
+  });
+});
+
